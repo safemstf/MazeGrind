910c5771dbbbb6e0ac653ed62761d28fb50b52b4
Demir Altay <77849115+daltay15@users.noreply.github.com>
2023-10-05 23:06:23 -0500

    Changed random number gen to have a ceiling of 21

--- Project 1/app.py
+++ Project 1/app.py
@@ -4,21 +4,24 @@ from PIL import ImageOps, Image
 import numpy as np
 app = Flask(__name__)
 
-MAZE_SIZE = (21,21)
+MAZE_SIZE = (21, 21)
+
+
 def read_maze_from_file(filename):
     with Image.open(filename) as im:
         im = im.resize(MAZE_SIZE)
-        #im.show()
+        # im.show()
     return np.array(ImageOps.grayscale(ImageOps.invert(im)))/255
+
 
 def convert_maze_to_list(maze):
     return list(list(int(y) for y in x) for x in maze)
 
+
 @app.route('/')
 def display_maze():
-    maze_id = random.randrange(1001)
-    maze = read_maze_from_file('archive/rectangular_mazes_10x10/'+str(maze_id)+'.png')
+    maze_id = random.randrange(21)
+    maze = read_maze_from_file(
+        'archive/rectangular_mazes_10x10/'+str(maze_id)+'.png')
     maze = convert_maze_to_list(maze)
     return render_template("amazement.html", maze=str(maze), maze_id=maze_id)
-
-
